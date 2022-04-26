5a92ace16ee7d0a20e870839b3d93734fda0b0af
Joseph Soria <josephsoria6@gmail.com>
2022-04-25 22:37:35 -0500

    Converted to a class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>This is a quiz app</h1>
-    </div>
-  );
+import React from "react";
+import HighscoreView from "./components/HighscoreView";
+import Homepage from "./components/Homepage";
+import MainView from "./components/MainView";
+import Quiz from "./components/Quiz";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the app component</h1>
+        <HighscoreView />
+        <Homepage />
+        <MainView />
+        <Quiz />
+      </div>
+    )
+  }
 }
 
 export default App;
