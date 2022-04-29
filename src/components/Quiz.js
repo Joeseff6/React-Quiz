a43fc58061001da50bf09b3a20d9e91ff9f97d83
Joseph Soria <josephsoria6@gmail.com>
2022-04-29 18:43:25 -0500

    Imported Quiz.css, added id and classes

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,33 +1,38 @@
 import React from "react";
 import axios from "axios";
+import "./Quiz.css";
 
 class Quiz extends React.Component {
   state = { questions: [], selectedQuestion: {} };
 
   async componentDidMount() {
     let response = await axios.get("http://localhost:5000/questions");
-    let questions = response.data, numberOfQuestions = questions.length;
+    let questions = response.data,
+      numberOfQuestions = questions.length;
     console.log(questions);
     let randomNumber = Math.floor(Math.random() * numberOfQuestions);
     let selectedQuestion = questions[randomNumber];
     console.log(selectedQuestion);
-    this.setState({ questions: questions, selectedQuestion: selectedQuestion})
+    this.setState({ questions: questions, selectedQuestion: selectedQuestion });
   }
 
   renderJSX() {
     if (!this.state.questions.length) return "Loading";
     return (
       <>
-        <h2>{this.state.selectedQuestion.question}</h2>
-        {this.state.selectedQuestion.choices.map(choice => {
+        <div id="question">
+          <h3 className="text-center">{this.state.selectedQuestion.question}</h3>
+        </div>
+
+        {this.state.selectedQuestion.choices.map((choice) => {
           return (
-            <h4 key={choice}>
-              {choice}
-            </h4>
-          )
+            <div className="choice" key={choice}>
+              <h4>{choice}</h4>
+            </div>
+          );
         })}
       </>
-    )
+    );
   }
 
   render() {
