1257bad750022018306ac8657184650ee5d7bc5b
Joseph Soria <josephsoria6@gmail.com>
2022-04-29 10:50:04 -0500

    Set up axios request on component mount

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,17 @@
-const Quiz = () => {
-  return(
-    <h1>This is the quiz component</h1>
-  )
+import React from "react";
+import axios from "axios";
+
+class Quiz extends React.Component {
+  async componentDidMount() {
+    let response = await axios.get("http://localhost:5000/questions");
+    let questions = response.data;
+    console.log(questions);
+
+  }
+
+  render() {
+    return <h1>This is the quiz component</h1>;
+  }
 }
 
 export default Quiz;
