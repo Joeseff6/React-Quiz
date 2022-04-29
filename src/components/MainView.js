d2c1d5041f72b40dc8ff3655b111403d4d08e71f
Joseph Soria <josephsoria6@gmail.com>
2022-04-29 09:37:51 -0500

    Removed Homepage, moved JSX to MainView

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,15 +1,57 @@
 import React from "react";
-import Homepage from "./Homepage";
 import Quiz from "./Quiz";
+import Collapse from "react-bootstrap/Collapse";
+
 
 class MainView extends React.Component {
+  state = { quizStart: false, dropIn: false };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({dropIn: true})
+    }, 750)
+  }
+
+  onQuizStart = () => {
+    setTimeout(() => {
+      this.setState({ quizStart: true });
+    }, 1000);
+  };
+
+
+
+  renderJSX() {
+    if (!this.state.quizStart) {
+      return (
+        <>
+          <div className="row justify-content-center mb-5">
+            <div className="col-8">
+              <h2 className="text-center">
+                Test your full stack and React.js knowledge in a race against
+                others to achieve the highest score in the shortest time!
+              </h2>
+            </div>
+          </div>
+          <Collapse in={this.state.dropIn}>
+            <div>
+              <button
+                className="btn option-button d-block m-auto mb-5"
+                onClick={this.onStartClick}
+              >
+                Start Quiz
+              </button>
+              <a className="btn option-button d-block m-auto">Highscore Page</a>
+            </div>
+          </Collapse>
+        </>
+      );
+    } else {
+      return <Quiz />;
+    }
+  }
+
   render() {
-    return(
-      <>
-      <Homepage />
-      <Quiz />
-      </>
-    )
+    return this.renderJSX();
   }
 }
 
