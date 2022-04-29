72a043e7594ebd81784becd12ca59b535ebc93b4
Joseph Soria <josephsoria6@gmail.com>
2022-04-29 11:05:25 -0500

    Made function renderJSX

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,15 +2,36 @@ import React from "react";
 import axios from "axios";
 
 class Quiz extends React.Component {
+  state = { questions: [], selectedQuestion: {} };
+
   async componentDidMount() {
     let response = await axios.get("http://localhost:5000/questions");
-    let questions = response.data;
+    let questions = response.data, numberOfQuestions = questions.length;
     console.log(questions);
+    let randomNumber = Math.floor(Math.random() * numberOfQuestions);
+    let selectedQuestion = questions[randomNumber];
+    console.log(selectedQuestion);
+    this.setState({ questions: questions, selectedQuestion: selectedQuestion})
+  }
 
+  renderJSX() {
+    if (!this.state.questions.length) return "Loading";
+    return (
+      <>
+        <h2>{this.state.selectedQuestion.question}</h2>
+        {this.state.selectedQuestion.choices.map(choice => {
+          return (
+            <h4 key={choice}>
+              {choice}
+            </h4>
+          )
+        })}
+      </>
+    )
   }
 
   render() {
-    return <h1>This is the quiz component</h1>;
+    return this.renderJSX();
   }
 }
 
