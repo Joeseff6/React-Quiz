417da4d936d3252cad11e306cd5202d0c718ebe6
Joseph Soria <josephsoria6@gmail.com>
2022-05-01 12:32:59 -0500

    Added index key to selectedQuestion

--- src/helper/selectQuestion.js
+++ src/helper/selectQuestion.js
@@ -2,6 +2,7 @@ const selectQuestion = (questions) => {
   let numberOfQuestions = questions.length;
   let randomNumber = Math.floor(Math.random() * numberOfQuestions);
   let selectedQuestion = questions[randomNumber];
+  selectedQuestion.index = randomNumber;
   return selectedQuestion;
 }
 
