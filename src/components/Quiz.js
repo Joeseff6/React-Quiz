80fac705adb28d878f51b2cc617a2fa62cdd8b6e
Joseph Soria <josephsoria6@gmail.com>
2022-05-01 01:36:35 -0500

    Removed timer state, wrote timer logic

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,17 +5,14 @@ import selectQuestion from "../helper/selectQuestion";
 import "./Quiz.css";
 
 class Quiz extends React.Component {
-  state = { questions: [], selectedQuestion: {}, score: 0, timer: 60 };
+  state = { questions: [], selectedQuestion: {}, score: 0 };
 
   async componentDidMount() {
     let response = await axios.get("http://localhost:5000/questions");
     let questions = response.data;
     let selectedQuestion = selectQuestion(questions);
     this.setState({ questions: questions, selectedQuestion: selectedQuestion });
-    setInterval(() => {
-      let newTime = this.state.timer - 1;
-      this.setState({ timer: newTime });
-    }, 1000);
+    this.runTimer();
   }
 
   onChoiceClick = (e) => {
@@ -24,10 +21,42 @@ class Quiz extends React.Component {
       let newScore = this.state.score + 1;
       this.setState({ score: newScore });
     } else {
-      let newTime = this.state.timer - 2;
-      this.setState({ timer: newTime });
     }
   };
+
+  runTimer = () => {
+    let time = 60;
+    document.getElementById("timer").innerHTML = `Time: ${time} secs`;
+    let timerId = setInterval(() => {
+      if (time <= 1) clearInterval(timerId);
+      document.getElementById("timer").innerHTML = `Time: ${time} ${
+        time > 1 ? "secs" : "sec"
+      }`;
+      time--;
+    }, 1000);
+  };
+
+  randomizeChoices = (choices) => {
+    let choicesCopy = choices.slice(0);
+    let choicesArray = [];
+    while (choicesCopy.length !== 0) {
+      let choiceLength = choicesCopy.length;
+      let randomIndex = Math.floor(Math.random() * choiceLength);
+      choicesArray.push(choicesCopy[randomIndex]);
+      choicesCopy.splice(randomIndex, 1);
+    }
+    return choicesArray.map((choice) => {
+      return (
+        <div
+          className="choice my-3 px-3 py-3 d-flex align-items-center"
+          key={choice}
+          onClick={(e) => this.onChoiceClick(e)}
+        >
+          {choice}
+        </div>
+      );
+    })
+  }
 
   renderJSX() {
     if (!this.state.questions.length) {
@@ -44,9 +73,7 @@ class Quiz extends React.Component {
               <h2 className="">Total score: {this.state.score}</h2>
             </div>
             <div className="col d-flex justify-content-center">
-              <h2>
-                Time: {this.state.timer} {this.state.timer > 1 ? "secs" : "sec"}
-              </h2>
+              <h2 id="timer"></h2>
             </div>
           </div>
           <div
@@ -57,18 +84,7 @@ class Quiz extends React.Component {
               {this.state.selectedQuestion.question}
             </h3>
           </div>
-
-          {this.state.selectedQuestion.choices.map((choice) => {
-            return (
-              <div
-                className="choice my-3 px-3 py-3 d-flex align-items-center"
-                key={choice}
-                onClick={(e) => this.onChoiceClick(e)}
-              >
-                {choice}
-              </div>
-            );
-          })}
+          {this.randomizeChoices(this.state.selectedQuestion.choices)}
         </>
       );
     }
