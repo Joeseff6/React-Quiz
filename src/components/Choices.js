d88c0eb0ce4e7b8f06bde4f89186dfb01c14f681
Joseph Soria <josephsoria6@gmail.com>
2022-05-08 13:32:48 -0500

    Passed new prop, moved function from Quiz

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,4 +1,4 @@
-const Choices = ({ selectedQuestion, onChoiceClick }) => {
+const Choices = ({ selectedQuestion, onChoiceClick, displayNextQuestion }) => {
   const randomizeChoices = (choices) => {
     let choicesCopy = choices.slice(0);
     let choicesArray = [];
@@ -9,6 +9,27 @@ const Choices = ({ selectedQuestion, onChoiceClick }) => {
       choicesCopy.splice(randomIndex, 1);
     }
     return choicesArray;
+  };
+
+  onChoiceClick = (e) => {
+    let selectedChoice = e.target.innerText;
+    let correctIndex = null;
+    let choiceElements = document.querySelectorAll(`.choice`);
+    choiceElements.forEach((element, index) => {
+      if (element.innerText === selectedQuestion.answer)
+        correctIndex = index;
+    });
+    document
+      .querySelector(`.choice[data-index="${correctIndex}"]`)
+      .classList.add("correct");
+    let correct = selectedChoice === selectedQuestion.answer;
+    setTimeout(() => {
+      // if (correct) {
+      //   let newScore = this.state.score + 1;
+      //   this.setState({ score: newScore });
+      // }
+      displayNextQuestion();
+    }, 1000);
   };
 
   const renderChoices = () => {
